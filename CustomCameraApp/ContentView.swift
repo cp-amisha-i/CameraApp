8bcd281c9936f9d84696fa1f4b4fba0cd842f7ab
Amisha <amisha.i@canopas.com>
2023-10-03 16:30:08 +0530

    Initial Commit

---
+++ CustomCameraApp/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  CustomCameraApp
+//
+//  Created by Amisha Italiya on 03/10/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ CustomCameraApp/CustomCameraAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomCameraAppApp.swift
+//  CustomCameraApp
+//
+//  Created by Amisha Italiya on 03/10/23.
+//
+
+import SwiftUI
+
+@main
+struct CustomCameraAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
